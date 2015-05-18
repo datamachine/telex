f94941c738f28bbc9b92862ae72bd233877d83ba
Phillip Lopo <philliplopo@gmail.com>
2015-05-18 11:49:29 -0700

    Converted TelegramPluginManager to a ConfigurablePluginManager.

--- TelegramPluginManager.py
+++ TelegramPluginManager.py
@@ -1,13 +1,21 @@
-from yapsy.PluginManager import PluginManager
+from yapsy.ConfigurablePluginManager import ConfigurablePluginManager
+import configparser
 
 
-class TelegramPluginManager(PluginManager):
+PLUGIN_CONFIG_NAME="plugins.conf"
+
+class TelegramPluginManager(ConfigurablePluginManager):
     def __init__(self, bot):
-        super().__init__()
+        self.config = configparser.ConfigParser()
+        super().__init__(configparser_instance = self.config, 
+                         config_change_trigger = lambda :self.save_config())
+
         self.bot = bot
 
-    def instanciateElement(self, element):
-        """
-        Override this method to customize how plugins are instanciated
-        """
-        return element(self.bot)
+    def save_config(self):
+        print("Saving plugins config...")
+        with open(PLUGIN_CONFIG_NAME, 'w') as f:
+            self.config.write(f)
+            return True
+        return False
+
