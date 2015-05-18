df69a6ce28ccf3ce5922e07d51e20fc207626140
Phillip Lopo <philliplopo@gmail.com>
2015-05-18 16:09:32 -0700

    adding weather emoji map

--- plugins/weather.py
+++ plugins/weather.py
@@ -5,6 +5,18 @@ sys.path.append('./plugins')
 
 from py3owm import OpenWeatherMap
 
+OWM_icon_emoji_map = {
+    "01d": u'\U0001F31E',
+    "02d": u'\U000026C5',
+    "03d": u'\U00002601',
+    "03d": u'\U00002601',
+    "09d": u'\U0001F302',
+    "10d": u'\U00002614',
+    "11d": u'\U000026A1',
+    "13d": u'\U00002744',
+    "50d": u'\U0001F301'
+}
+
 class WeatherPlugin(plugintypes.TelegramPlugin):
     """
     Weather
@@ -15,7 +27,7 @@ class WeatherPlugin(plugintypes.TelegramPlugin):
     ]
 
     usage = [
-        "!weather <zip code>|<city>"
+        "!weather <zip code>"
     ]
 
     def activate_plugin(self):
