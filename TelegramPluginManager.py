e0a8934594b2fee27c7c5c9e5e25c94e56bc71a2
Phillip Lopo <philliplopo@gmail.com>
2015-05-18 12:49:03 -0700

    Loading plugins config when plugin manager loads

--- TelegramPluginManager.py
+++ TelegramPluginManager.py
@@ -7,6 +7,8 @@ PLUGIN_CONFIG_NAME="plugins.conf"
 class TelegramPluginManager(ConfigurablePluginManager):
     def __init__(self, bot):
         self.config = configparser.ConfigParser()
+        self.config.read(PLUGIN_CONFIG_NAME)
+
         super().__init__(configparser_instance = self.config, 
                          config_change_trigger = lambda :self.save_config(),
                          directories_list = ["plugins"] )
@@ -14,7 +16,6 @@ class TelegramPluginManager(ConfigurablePluginManager):
         self.bot = bot
 
     def save_config(self):
-        print("Saving plugins config...")
         with open(PLUGIN_CONFIG_NAME, 'w') as f:
             self.config.write(f)
             return True
