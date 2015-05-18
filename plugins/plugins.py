1148784e24f09e2daec66749e58f66a50327a39f
Phillip Lopo <philliplopo@gmail.com>
2015-05-18 12:59:53 -0700

    plugins.py now uses the plugin manager directly for plugin management

--- plugins/plugins.py
+++ plugins/plugins.py
@@ -33,14 +33,17 @@ class PluginsPlugin(plugintypes.TelegramPlugin):
                 return "Error loading plugin {}".format(matches.group(2))
 
         if matches.group(1) == "disable":
-            return self.bot.disable_plugin(matches.group(2))
+            if self.plugin_manager.activatePluginByName(matches.group(2)):
+                return "Plugin {} disabled".format(matches.group(2))
+            else:
+                return "Error disabling plugin {}".format(matches.group(2))
 
         if matches.group(1) == "reload":
-            self.bot.load_plugins()
+            self.plugin_manager.collectPlugins()
 
     def list_plugins(self):
         text = ""
-        for plugin in self.bot.get_plugins():
+        for plugin in self.plugin_manager.getAllPlugins():
             text += "{0}: ({1})\n".format(plugin.name,
                                           "(Enabled)" if plugin.plugin_object.is_activated else "(Disabled)")
 
