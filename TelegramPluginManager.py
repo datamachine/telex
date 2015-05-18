cc163829a10cbfca599638501696add677a25acb
Phillip Lopo <philliplopo@gmail.com>
2015-05-18 12:24:59 -0700

    plugins now recieve an instance of the plugin manager

--- TelegramPluginManager.py
+++ TelegramPluginManager.py
@@ -25,6 +25,7 @@ class TelegramPluginManager(ConfigurablePluginManager):
 
         if plugin_object:
             plugin_object.set_bot(self.bot)
+            plugin_object.set_plugin_manager(self)
             return plugin_object
         
         return None
--- plugintypes.py
+++ plugintypes.py
@@ -4,9 +4,13 @@ class TelegramPlugin(IPlugin):
     def __init__(self):
         super().__init__()
         self.bot = None
+        self.plugin_manager = None
 
     def set_bot(self, bot):
         self.bot = bot
+
+    def set_plugin_manager(self, plugin_manager):
+        self.plugin_manager = plugin_manager
 
     def run(self, msg, matches):
         raise NotImplementedError
