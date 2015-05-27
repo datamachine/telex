50caf100988aa1dde77512a3f19500feeaf3e27c
Phillip Lopo <philliplopo@gmail.com>
2015-05-27 14:57:37 -0700

    first set of unit tests working for git clone wrapper

--- telegrambot/git/git.py
+++ telegrambot/git/git.py
@@ -1,26 +1,42 @@
 import shutil
 from subprocess import Popen
 from tempfile import TemporaryFile
+from urllib.parse import urlparse
+from pathlib import PurePath
+from urllib.parse import unquote
 
 GIT_BIN=shutil.which("git")
+
+
+# Determine the default directory name used by git for the uri
+def get_default_repo_dirname(uri, bare=False):
+    path = PurePath(urlparse(uri)[2])
+    name = unquote(path.parts[-1])
+    if not bare and path.suffix == ".git":
+        return name[0:-4]
+
+    if bare and path.suffix != ".git":
+        return "{}.git".format(name)
+
+    return name
 
 class GitStatus:
     def __init__(self, exit_status, stdout, stderr):
         self.exit_status = exit_status
         self.stdout = stdout
-        seld.stderr = stderr
+        self.stderr = stderr
 
 def clone(cwd, repository, directory):
-    args = [self.git_bin, "clone", "--", repository, directory]
+    args = [GIT_BIN, "clone", "--", repository]
+    if directory: args += [directory]
 
-    out, err = TemporaryFile(), TemporaryFile()
-    p = Popen(args, cwd=cwd, stdout=out, stderr=err)
-    status = p.wait()
+    with TemporaryFile() as outf, TemporaryFile() as errf:
+        p = Popen(args, cwd=cwd, stdout=outf, stderr=errf)
+        status = p.wait()
 
-    out.seek(0)
-    err.seek(0)
+        outf.seek(0)
+        errf.seek(0)
 
-    return GitResponse(status, out.read().decode('utf-8'), err.read().decode('utf-8'))
+        return GitStatus(status, outf.read().decode('utf-8'), errf.read().decode('utf-8'))
+    return None
 
-if __name__ == "__main__":
-    print("it works")    
--- telegrambot/git/unittests.py
+++ telegrambot/git/unittests.py
@@ -1,12 +1,43 @@
 import unittest
 from tempfile import TemporaryDirectory
+from os import path
+from urllib.parse import urlparse
+
 import telegrambot.git as git
 
+REPO_URL = "https://github.com/datamachine/telegram-pybot-dice.git"
+REPO_NAME = "dice"
+REPO_DEFAULT_NAME="telegram-pybot-dice"
+REPO_BARE_NAME="telegram-pybot-dice.git"
+
+class GetDefaultRepoDirnameTest(unittest.TestCase):
+    def test_default(self):
+        self.assertEqual(git.get_default_repo_dirname("https://github.com/datamachine/telegram-pybot-dice.git"), "telegram-pybot-dice")
+        self.assertEqual(git.get_default_repo_dirname("https://github.com/datamachine/telegram-pybot-dice"), "telegram-pybot-dice")
+        self.assertEqual(git.get_default_repo_dirname("git@github.com:datamachine/telegram-pybot-dice.git"), "telegram-pybot-dice")
+
+    def test_default_bare(self):
+        self.assertEqual(git.get_default_repo_dirname("https://github.com/datamachine/telegram-pybot-dice.git", bare=True), "telegram-pybot-dice.git")
+        self.assertEqual(git.get_default_repo_dirname("https://github.com/datamachine/telegram-pybot-dice", bare=True), "telegram-pybot-dice.git")
+        self.assertEqual(git.get_default_repo_dirname("git@github.com:datamachine/telegram-pybot-dice.git", bare=True), "telegram-pybot-dice.git")
+
 class CloneTest(unittest.TestCase):
-    def test_new(self):
-        pass
-        #with TemporaryDirectory() as d:
+    def test_clone_to_empty_with_default_name(self):
+        with TemporaryDirectory() as d:
+            gs = git.clone(cwd=d, repository=REPO_URL, directory=None)
+            self.assertIsNotNone(gs)
+            self.assertTrue(path.exists(path.join(d, REPO_DEFAULT_NAME, ".git")))
+            self.assertEqual(gs.exit_status, 0)
+        
+    def test_clone_to_empty_with_name(self):
+        with TemporaryDirectory() as d:
+            gs = git.clone(cwd=d, repository=REPO_URL, directory=REPO_NAME)
+            self.assertIsNotNone(gs)
+            self.assertTrue(path.exists(path.join(d, REPO_NAME, ".git")))
+            self.assertEqual(gs.exit_status, 0)
+
+            
         
 if __name__ == "__main__":
-    print(git.clone)
-    print("test") 
+    unittest.main()
+
