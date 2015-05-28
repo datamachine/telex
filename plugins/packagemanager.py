3b85b05a34a546760646e8fc225cf27e8c328538
Vincent Castellano <surye80@gmail.com>
2015-05-27 20:11:46 -0700

    Don't manually run gen

--- plugins/packagemanager.py
+++ plugins/packagemanager.py
@@ -59,11 +59,6 @@ class PackageManagerPlugin(plugintypes.TelegramPlugin):
 
         try:
             central_repo_path = path.join(PKG_REPO_DIR, CENTRAL_REPO_DIR)
-            f = TemporaryFile()
-            args = ["./generate_repo.py"]
-            p = subprocess.Popen(args, cwd=central_repo_path, stdout=f, stderr=f)
-            p.wait()
-
             with open(path.join(PKG_REPO_DIR, CENTRAL_REPO_DIR, "repo.json"), "r") as f:
                 self.central_repo = json.load(f)
         except:
