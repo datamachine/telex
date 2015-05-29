4214802cb3105ea58ce7555b4f829d132a474db6
Vincent Castellano <surye80@gmail.com>
2015-05-28 22:20:06 -0700

    Still not working, need to re-evaluate this plugin.

--- plugins/media.py
+++ plugins/media.py
@@ -6,7 +6,7 @@ class MediaPlugin(plugintypes.TelegramPlugin):
     Upload media files to chat when linked.
     """
     patterns = [
-        "(https?://[\w\-\_\.\?\:\/\+\=\&]+\.(gif|mp4|pdf|pdf|ogg|zip|mp3|rar|wmv|doc|avi))v?",
+        "(https?://[\w\-\_\.\?\:\/\+\=\&]+\.(gifv|gif|mp4|pdf|pdf|ogg|zip|mp3|rar|wmv|doc|avi))",
     ]
 
     usage = [
@@ -14,6 +14,9 @@ class MediaPlugin(plugintypes.TelegramPlugin):
     ]
 
     def run(self, msg, matches):
+        print (matches.group(1))
+        if matches.group(1).endswith("gifv"):
+            return
         filename = self.bot.download_to_file(matches.group(1), matches.group(2))
         if filename:
            peer = self.bot.get_peer_to_send(msg)
