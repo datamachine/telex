295bbaa91d4b0eac363b6dd44973b8b97fdd9222
Phillip Lopo <philliplopo@gmail.com>
2015-05-31 00:40:28 -0700

    changing support link

--- plugins/support.py
+++ plugins/support.py
@@ -13,7 +13,6 @@ class SupportPlugin(plugin.TelegramPlugin):
         contact_info = [
         "Bugs and issues can be reported at: https://github.com/datamachine/telegram-pybot/issues",
         "Or you can contact the devs directly at:",
-        "https://telegram.me/Surye",
-        "https://telegram.me/Tyrannosaurus"
+        "https://telegram.me/joinchat/05c5c2f60112fa104d1c0c563b2fd34a",
         ]
         return "\n".join(contact_info)
