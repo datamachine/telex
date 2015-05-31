8e7a8085ae46f761286be1324ba76f3e99055454
Phillip Lopo <philliplopo@gmail.com>
2015-05-30 19:38:41 -0700

    Deprecated warning for plugins using plugintypes

--- plugintypes.py
+++ plugintypes.py
@@ -1,43 +1,12 @@
-from yapsy.IPlugin import IPlugin
+from telegrambot import plugin
 
-class TelegramPlugin(IPlugin):
+class TelegramPlugin(plugin.TelegramPlugin):
     def __init__(self):
+        print("DEPRECATED!!! Use module telegrambot.plugin instead of plugintypes")
         super().__init__()
-        self.bot = None
-        self.plugin_manager = None
-        self.name = None
-        self.category_name = None
-
-    def set_bot(self, bot):
-        self.bot = bot
-
-    def set_plugin_manager(self, plugin_manager):
-        self.plugin_manager = plugin_manager
 
     def set_name(self, name):
-        self.name = name
-
-    def set_category_name(self, category_name):
-        self.category_name = category_name
-
-    def write_option(self, option, value):
-        self.plugin_manager.registerOptionFromPlugin(self.category_name, self.name, option, value)
-
-    def read_option(self, option):
-        return self.plugin_manager.readOptionFromPlugin(self.category_name, self.name, option)
-
-    def has_option(self, option):
-        return self.plugin_manager.hasOptionFromPlugin(self.category_name, self.name, option)
-
-    def activate_plugin(self):
-        pass
-
-    def run(self, msg, matches):
-        raise NotImplementedError
-
-    def pre_process(self, msg):
-        pass
-
-    def respond_to_msg(self, src_msg, msg_text, **kwargs):
-        self.bot.get_peer_to_send(src_msg).send_msg(msg_text, **kwargs)
+        print("DEPRECATED!!! {}: Use module telegrambot.plugin instead of plugintypes".format(name))
+        super().set_name(name)
+        
 
