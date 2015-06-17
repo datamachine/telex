0ed8429e03c63cc9d0eaabf916d70f5de672b2ec
Phillip Lopo <philliplopo@gmail.com>
2015-06-16 21:19:28 -0700

    Removing old decorators used in filters

--- telex/callbacks/filters.py
+++ telex/callbacks/filters.py
@@ -1,23 +1,3 @@
 from functools import wraps
 
-from .callback import callback
-
-def match(expr):
-    import re
-    prog = re.compile(expr)
-    def _match(func):
-        def match_wrapper(*args, msg, **kwargs):
-            if prog.match(msg.text):
-                return func(*args, msg=msg, **kwargs)
-            return None
-        return match_wrapper
-    return _match
-
-def command(command, *, aliases=[]):
-    def _command(func):
-        setattr(func, '_telex_filter_command', True)
-        def command_wrapper(*args, msg, **kwargs):
-            return func(*args, msg=msg, **kwargs)
-        return command_wrapper
-    return _command
 
