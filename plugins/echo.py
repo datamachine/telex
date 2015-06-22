6723855ea556e519ef982b5aa7962e65da4221fc
Syed Nisar Ul Haq <nisarul@gmail.com>
2015-06-22 23:05:29 +0530

    Limited maximum count o 20

--- plugins/echo.py
+++ plugins/echo.py
@@ -8,5 +8,8 @@ class EchoPlugin(plugin.TelexPlugin):
     def test_callback(self, count, text, *, bot, msg):
         if not count:
             count = 1
-        for i in range(0,int(count)):
-            self.respond_to_msg(msg, text)
+        if count > 20:
+            self.respond_to_msg(msg, "Maximum count is 20")
+        else
+            for i in range(0,int(count)):
+                self.respond_to_msg(msg, text)
